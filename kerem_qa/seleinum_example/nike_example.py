ca41e14fe55dce8dbaf7e59db14d83b8375ded24
User <lunahasson64@gmail.com>
2025-12-08 09:16:14 +0200

    first commit

---
+++ kerem_qa/seleinum_example/nike_example.py
@@ -0,0 +1,17 @@
+import time
+from time import sleep
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
+
+print("Test start")
+service = ChromeService(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
+
+driver.maximize_window()
+driver.implicitly_wait(10)
+
+driver.get('https://www.nike.com/il/')
+Find_a_Store_link = driver.find_element(By.LINK_TEXT, "Find a Store")
+Find_a_Store_link.click()
